f96e2a7370279f3033977144cd2f7f47693a81b3
theking250902 <buivanvietx5hongloc@gmail.com>
2023-07-19 22:48:08 +0700

    login

---
+++ app/src/main/java/com/example/my_app/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.my_app.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.my_app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
