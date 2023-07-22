60ce57ccee96598369736cb81233e8242de2bea5
theking250902 <buivanvietx5hongloc@gmail.com>
2023-07-22 22:15:27 +0700

    login

---
+++ app/src/main/java/com/example/my_app/Home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.my_app.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.my_app.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
