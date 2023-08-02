ad9fcf889f5ce4944795bc29745c9706c23e7759
ThChien123 <chientran02012003@gmail.com>
2023-08-03 00:59:56 +0700

    chien
    code giao diện fragment cá nhân

--- app/src/main/java/com/example/my_app/fragment/canhanFragment.java
+++ app/src/main/java/com/example/my_app/fragment/canhanFragment.java
@@ -1,5 +1,6 @@
 package com.example.my_app.fragment;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,38 +8,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.my_app.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link canhanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class canhanFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public canhanFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment canhanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static canhanFragment newInstance(String param1, String param2) {
         canhanFragment fragment = new canhanFragment();
         Bundle args = new Bundle();
@@ -46,6 +37,7 @@ public class canhanFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     @Override
@@ -55,12 +47,19 @@ public class canhanFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_canhan, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_canhan, container, false);
+
+        ImageView imageView = rootView.findViewById(R.id.img_avt);
+        Glide.with(this)
+                .load(R.drawable.anhdaidien)
+                .apply(new RequestOptions().transform(new CenterCrop()).transform(new RoundedCorners(45)))
+                .into(imageView);
+        return rootView;
     }
 }
